f9fbe89929fc0e7f82fa26d07f52834541fe949b
Tiago&Tiago <tromacho2004@gmail.com>
2025-10-06 18:05:56 +0100

    Adicionamos Divisao

--- calca.py
+++ calca.py
@@ -1,7 +1,7 @@
 def calculadora():
-    print("Calculadora Simples - Soma, Subtração e Multiplicação")
+    print("Calculadora Simples - Soma, Subtração, Multiplicação e Divisão")
     num1 = float(input("Digite o primeiro número: "))
-    operacao = input("Digite a operação (+, -, *): ")
+    operacao = input("Digite a operação (+, -, *, /): ")
     num2 = float(input("Digite o segundo número: "))
 
     if operacao == '+':
@@ -13,8 +13,13 @@ def calculadora():
     elif operacao == '*':
         resultado = num1 * num2
         print(f"Resultado: {num1} * {num2} = {resultado}")
+    elif operacao == '/':
+        if num2 != 0:
+            resultado = num1 / num2
+            print(f"Resultado: {num1} / {num2} = {resultado}")
+        else:
+            print("Erro: divisão por zero não é permitida.")
     else:
-        print("Operação inválida. Use +, - ou *.")
+        print("Operação inválida. Use +, -, * ou /.")
 
 calculadora()
-
